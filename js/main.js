e752c9cae2ce93057670d67a9e647acdf56ed3b5
Yevhenii <zhenia1717@gmail.com>
2022-07-18 23:49:18 +0300

    add smooth scroll and pageup

--- js/main.js
+++ js/main.js
@@ -7,17 +7,19 @@ $(function () {
         autoplaySpeed: 2000,
     });
     $(".menu, bike__link").on("click", "a", function (event) {
-        //отменяем стандартную обработку нажатия по ссылке
         event.preventDefault();
-
-        //забираем идентификатор бока с атрибута href
         var id = $(this).attr('href'),
-
-            //узнаем высоту от начала страницы до блока на который ссылается якорь
             top = $(id).offset().top;
-
-        //анимируем переход на расстояние - top за 1500 мс
         $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
+    // Smooth scroll and pageup
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.page-up').fadeIn();
+        } else {
+            $('.page-up').fadeOut();
+        }
     });
 
 }); 
